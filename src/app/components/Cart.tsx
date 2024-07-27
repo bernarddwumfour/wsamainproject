6b1ded1e084b41cbb461747e9e6c31024d8d801c
bernarddwumfour <bernarddwumfour25@gmail.com>
2024-07-27 23:32:47 +0000

    Fixed key error

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -8,7 +8,7 @@ const Cart = () => {
   return (
     <div className={`${cartopen?"block":"hidden"} absolute py-2 bg-white top-24 p-1  right-12 shadow-lg`}>
         <p className='text-center font-semibold text-[1rem] p-2'>Products In Cart</p>
-        {cart && cart.length>0 && cart.map(product=>(<Cartitem product={product}/>))}
+        {cart && cart.length>0 && cart.map(product=>(<Cartitem key={product.id} product={product}/>))}
         <div className="text-center bg-gray-600 text-white p-2 text-[1rem] rounded-sm mt-4">Go To CheckOut</div>
     </div>
   )
