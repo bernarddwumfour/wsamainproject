8bc425904e8d3bf07efef9fdcd33666fa56346e8
bernarddwumfour <bernarddwumfour25@gmail.com>
2024-07-29 11:43:24 +0000

    Fixing login

---
+++ src/app/api/shops/route.ts
@@ -0,0 +1,46 @@
+import connectMongo from "@/app/database/db"
+import Shop from "@/app/database/models/Shop";
+import { NextResponse } from "next/server"
+
+export async function GET() {
+    try {
+      await connectMongo();
+  
+      const shops = await Shop.find();
+  
+      if (shops && shops.length > 0) {
+        return NextResponse.json({ shops }, { status: 200 });
+      } else {
+        return NextResponse.json({ message: 'No shops found' }, { status: 404 });
+      }
+    } catch (error) {
+      console.error('Error fetching shops:', error);
+      return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }``
+  }
+
+
+  export async function POST(request: Request) {
+    try {
+      const { name, address } = await request.json();
+  
+      if (!(name && address)) {
+        return NextResponse.json({ message: 'Invalid shop data' }, { status: 400 });
+      }
+  
+      await connectMongo();
+      const shop = await Shop.create({
+          name,
+          address
+      });
+  
+      if (shop) {
+        return NextResponse.json({ message: 'shop created successfully' }, { status: 201 });
+      } else {
+        return NextResponse.json({ message: 'shop creation failed' }, { status: 500 });
+      }
+    } catch (error) {
+      console.error('Error creating shop:', error);
+      return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
+  }
